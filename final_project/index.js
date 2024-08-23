0ada5ef2461c38478191f9c771f52455e28592a5
EdenGebremedhin <edhabtetsion@gmail.com>
2024-08-23 18:56:44 -0400

    routes1-4

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,31 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+
+const authHeader = req.headers['authorization'];
+    
+    // Check if the Authorization header is provided and has the Bearer token
+    if (authHeader && authHeader.startsWith('Bearer ')) {
+        // Extract the token part (after 'Bearer ')
+        const token = authHeader.split(' ')[1];
+        
+        // Verify the token using jsonwebtoken
+        jwt.verify(token, "your_jwt_secret_key", (err, decoded) => {
+            if (err) {
+                // Token is invalid or expired
+                return res.status(401).send("Invalid or expired token");
+            } else {
+                // Token is valid, store the decoded information in req.user
+                req.user = decoded;
+                next(); // Proceed to the next middleware or route handler
+            }
+        });
+    } else {
+        // If no token is provided, return an unauthorized response
+        return res.status(401).send("Access Denied: No Token Provided");
+    }
+
+
 });
  
 const PORT =5000;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,26 +12,69 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books,null,4));
+
+//   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    // res.send(books[isbn]);
+
+  // Check if the book with the given ISBN exists
+    const book = books[isbn];
+    if (book) {
+        // Return the book information if found
+        return res.status(200).json(book);
+    } else {
+        // Return a 404 error if the book is not found
+        return res.status(404).json({ message: "Book not found" });
+    }
+//   return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    // Extract the author parameter from the request URL
+    const author = req.params.author;
+
+    // Obtain all keys for the 'books' object
+    const bookKeys = Object.keys(books);
+
+    // Filter the books by author
+    let filtered_books = bookKeys.map(key => books[key]).filter(book => book.author === author);
+
+    // Check if any books were found for the given author
+    if (filtered_books.length > 0) {
+        // Send the filtered_books array as the response to the client
+        res.send(filtered_books);
+    } else {
+        // If no books were found, send a 404 response with a message
+        res.status(404).json({ message: "No books found by this author" });
+    }
 });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+
+    // Obtain all keys for the 'books' object
+    const bookKeys = Object.keys(books);
+
+    // Filter the books by author
+    let filtered_books = bookKeys.map(key => books[key]).filter(book => book.title === title);
+
+    // Check if any books were found for the given author
+    if (filtered_books.length > 0) {
+        // Send the filtered_books array as the response to the client
+        res.send(filtered_books);
+    } else {
+        // If no books were found, send a 404 response with a message
+        res.status(404).json({ message: "No books found with this title" });
+    }
+//   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
